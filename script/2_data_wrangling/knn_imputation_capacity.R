cde59d7fa6318467fb94f66b0963f3e8af495165
PomegranateJuice <jenniferzhangjiani@gmail.com>
2025-07-08 23:32:09 -0400

    KNN imputation for Capacity and Vulnerability

---
+++ script/2_data_wrangling/knn_imputation_capacity.R
@@ -0,0 +1,39 @@
+# ----------------------------------------
+# R Script to fill in missing data with K Nearest Neighbors imputation for Capacity Module
+# Author: Jennifer Zhang
+# Last edited: 2025-06-25
+# ----------------------------------------
+
+library(sf)
+library(dplyr)
+library(VIM)
+library(ggplot2)
+
+# Read in data from module
+data <- st_read('data/output/nc_grid_capacity_transformed_no_impute.gpkg')
+
+# Put centroids in a second geometry column
+data$centroids <- st_centroid(data$geom)
+coords <- st_coordinates(data$centroids)
+
+# Add coordinates
+data$x_coord <- coords[, 1]
+data$y_coord <- coords[, 2]
+
+# List of variable columns to impute
+cols <- names(data)[4:24] # adjust for other modules
+
+# Impute data with K Nearest Neighbors algorithm
+imputed_data <- kNN(data,
+                    variable=cols,
+                    k=5, # can be adjusted
+                    dist_var=c("x_coord","y_coord"),
+                    imp_var=FALSE)
+
+data[cols] <- imputed_data[cols]
+
+# Check results
+# plot(data['pct_poverty'], border=NA)
+# colSums(is.na(data))
+
+st_write(data, "data/output/nc_grid_capacity_imputed.gpkg", layer="capacity", delete_layer=TRUE)
